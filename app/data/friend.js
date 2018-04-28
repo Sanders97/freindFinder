6e657b70b4f27e0a820751cd850f58fd9077f28b
Marsalis <mrwsanders1@gmail.com>
2018-04-28 16:46:50 -0400

    almost done

---
+++ app/data/friend.js
@@ -0,0 +1,85 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Angerey',
+		photo: 'https://vignette.wikia.nocookie.net/fallout/images/d/d0/Fo4_Cait.jpg/revision/latest?cb=20160131023843',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Floaty Boi',
+		photo: 'https://vignette.wikia.nocookie.net/fallout/images/1/12/Codsworth_E3_PostWar.png/revision/latest?cb=20150616000527',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Buzz Lightyear',
+		photo: 'https://vignette.wikia.nocookie.net/fallout/images/d/d6/Danse.jpg/revision/latest?cb=20151211181324',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Paper',
+		photo: 'https://vignette.wikia.nocookie.net/fallout/images/4/42/Fo4_Piper_cropped.png/revision/latest?cb=20160329041731',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Big Green',
+		photo: 'https://vignette.wikia.nocookie.net/fallout/images/e/eb/Strong.png/revision/latest?cb=20151214073924',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+];
+
+module.exports = friendsArray;
